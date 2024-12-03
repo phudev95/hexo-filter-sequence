d6150a01f45b744612f5c80815fa4d70a1c538ac
Paolo Phan <paolophandev@gmail.com>
2024-12-04 00:53:53 +0700

    Correct config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var assign = require('deep-assign');
 var renderer = require('./lib/renderer');
 
-hexo.config.flowchart = assign({
+hexo.config.sequence = assign({
   webfont: 'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.27/webfontloader.js',
   snap: 'https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.4.1/snap.svg-min.js',
   underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
@@ -10,7 +10,7 @@ hexo.config.flowchart = assign({
   options: {
     theme: 'simple'
   }
-}, hexo.config.flowchart);
+}, hexo.config.sequence);
 
 hexo.extend.filter.register('before_post_render', renderer.render, 9);
 
